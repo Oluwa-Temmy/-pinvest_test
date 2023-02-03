d9d67fc31ee1943f2a78c004102e6f5d135c1252
Oluwa-Temmy <114503158+Oluwa-Temmy@users.noreply.github.com>
2023-02-02 21:03:07 -0500

    Delete and Added new file startup project
    restarted the project. and started it again

--- pin_site/manage.py
+++ pin_site/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pinvestigate.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pin_site.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
