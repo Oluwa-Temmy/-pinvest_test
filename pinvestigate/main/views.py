ab1dc63855fc3c3ba8de47e58ab06c0858fccd7f
Oluwa-Temmy <114503158+Oluwa-Temmy@users.noreply.github.com>
2023-03-19 23:12:36 -0400

    Template adjustments and file structure update
    Current objective: Make sure to do models for user registration. Make folder for models.
    
    New login in and sign up page
    Fixed index

--- pinvestigate/main/views.py
+++ pinvestigate/main/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+#*For user signup
+from django.contrib.auth.forms import UserCreationForm
+
 # Create your views here.
 def index(response):
     return render(response, "main/index.html", {})
+
+def login(response):
+    return render(response, "main/access_forms_html/LoginPage.html", {})
+
+def signup(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form': form}
+    return render(request, "main/access_forms_html/SignupPage.html", context)
 
